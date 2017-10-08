73d32cdd35c0012c363ef94faf564da8a2482b77
jhonychamorro19 <j.chamorro.reinoso@outlook.com>
2017-10-08 13:02:23 -0500

    Agregar archivos de proyecto.

---
+++ AM_1/AM_1/MasterPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AM_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : ContentPage
+    {
+        public ListView ListView { get { return listView; } }
+        public MasterPage()
+        {
+            InitializeComponent();
+
+            var masterPageItem = new List<MasterPageItem>();
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "HomePage",
+                IconSource = "fox.png",
+                TargetType = typeof(HomePage)
+            });
+            
+            masterPageItem.Add(new MasterPageItem
+            {
+                //Quienes somos
+                Title = "AboutUsPage",
+                IconSource = "home.png",
+                TargetType = typeof(AboutUsPage)
+            });
+            
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "ProductsPage",
+                IconSource = "",
+                TargetType = typeof(ProductsPage)
+            });
+            
+            masterPageItem.Add(new MasterPageItem
+            {
+                //Sucursales
+                Title = "BranchPage",
+                IconSource = "",
+                TargetType = typeof(BranchPage)
+            });
+           
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "NewsPage",
+                IconSource = "",
+                TargetType = typeof(NewsPage)
+            });
+           
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "ContactPage",
+                IconSource = "",
+                TargetType = typeof(ContactPage)
+            });
+
+            listView.ItemsSource = masterPageItem;
+        }
+    }
+}
