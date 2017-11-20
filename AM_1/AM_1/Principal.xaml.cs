d49425050e0ebd01f23b34c842cd6f5cdbb41a84
jhonychamorro19 <j.chamorro.reinoso@outlook.com>
2017-11-20 17:00:53 -0500

    no se que hice pero arreglé el error xD

--- AM_1/AM_1/Principal.xaml.cs
+++ AM_1/AM_1/Principal.xaml.cs
@@ -12,9 +12,43 @@ namespace AM_1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        public class Datos
+        {
+            public string img { get; set; }
+
+            public string texto { get; set; }
+        }
+
+        List<Datos> lista;
+
         public HomePage()
         {
             InitializeComponent();
+
+            lista = new List<Datos>();
+
+            lista.Add(new Datos
+            {
+                img = "campo1.jpg",
+                texto = "Elige el mejor campo"
+            });
+            lista.Add(new Datos
+            {
+                img = "campo2.jpg",
+                texto = "Juega con los mejore"
+            });
+            lista.Add(new Datos
+            {
+                img = "campo3.jpg",
+                texto = "Ofertas"
+            });
+            lista.Add(new Datos
+            {
+                img = "campo3.gif",
+                texto = "Holi"
+            });
+
+            car.ItemsSource = lista;
         }
     }
 }
